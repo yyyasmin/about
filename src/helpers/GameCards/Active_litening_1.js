aafda2b20f1bffabe94d75ec67696bb7d13214b9
unknown <you@example.com>
2024-02-27 09:33:40 +0200

    all

---
+++ src/helpers/GameCards/Active_litening_1.js
@@ -0,0 +1,29 @@
+// Import all the required images for Emotions-1
+
+export const roomSelectionBackgroundImage = require("../../assets/textures/active_listening_1/activeLitening.png");
+
+export const lis1_1 = require("../../assets/textures/active_listening_1/png1/lis1.png");
+export const lis1_2 = require("../../assets/textures/active_listening_1/png2/lis1.png");
+
+export const lis2_1 = require("../../assets/textures/active_listening_1/png1/lis2.png");
+export const lis2_2 = require("../../assets/textures/active_listening_1/png2/lis2.png");
+
+export const lis3_1 = require("../../assets/textures/active_listening_1/png1/lis3.png");
+export const lis3_2 = require("../../assets/textures/active_listening_1/png2/lis3.png");
+
+export const lis4_1 = require("../../assets/textures/active_listening_1/png1/lis4.png");
+export const lis4_2 = require("../../assets/textures/active_listening_1/png2/lis4.png");
+
+export const lis5_1 = require("../../assets/textures/active_listening_1/png1/lis5.png");
+export const lis5_2 = require("../../assets/textures/active_listening_1/png2/lis5.png");
+
+export const Active_litening_1 = [
+  roomSelectionBackgroundImage,  // UPDATE INIT , RoomsList, RoomItem 
+
+  [lis1_1, lis1_2],
+  [lis2_1, lis2_2],
+  [lis3_1, lis3_2],
+  [lis4_1, lis4_2],
+  [lis5_1, lis5_2],
+
+];
