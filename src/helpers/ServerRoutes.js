cdc8e350163044d6d9db1bfe11a5bcc895123a51
unknown <you@example.com>
2024-02-27 09:44:42 +0200

    URL

--- src/helpers/ServerRoutes.js
+++ src/helpers/ServerRoutes.js
@@ -1,5 +1,5 @@
 export const PROXY_URL = "http://localhost:5000";
-export const RENDER_PROXY_URL = "https://mg-about-production.up.railway.app";
+export const RENDER_PROXY_URL = "https://about-production-170c.up.railway.app";
 
 //export const CHOSEN_PROXY_URL = RENDER_PROXY_URL;
 export const CHOSEN_PROXY_URL = PROXY_URL;
